d8546cb66031863b4663e9c34969355230ebbdef
pooooobi <cyh4662@icloud.com>
2025-04-28 21:30:54 +0900

    initial commit

---
+++ src/main/java/com/charger/ocpp/OcppApplication.java
@@ -0,0 +1,13 @@
+package com.charger.ocpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OcppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/charger/ocpp/OcppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charger.ocpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
